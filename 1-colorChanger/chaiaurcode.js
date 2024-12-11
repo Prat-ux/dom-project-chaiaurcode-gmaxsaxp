8de311384ce303796a9dcf489e52dba0148b92bc
Prat-ux <pratyushyadav0106@gmail.com>
2024-12-11 17:59:32 +0530

    Initial commit

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,10 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    log.console(e);
+    log.console(e.target);
+  });
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# dom-project-chaiaurcode-gmaxsaxp
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Prat-ux/dom-project-chaiaurcode-gmaxsaxp)
